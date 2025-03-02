e0e70c8e4b021d4382392d4ab1f2ded67566f802
Ron Shani <ronshan4u@gmail.com>
2025-03-02 17:12:59 +0200

    refactoring

--- examples/hello_quad/main.py
+++ examples/hello_quad/main.py
@@ -3,7 +3,7 @@ from cobra import *
 def main():
     wnd = Window("Game", Vector2(800, 600))
 
-    m = Mesh([
+    quad = Mesh([
         Vector3( 1,  1, 0),
         Vector3( 1, -1, 0),
         Vector3(-1, -1, 0),
@@ -12,7 +12,7 @@ def main():
         0, 1, 3,
         1, 2, 3
     ])
-    s = Shader("./assets/shaders/unlit.vert", "./assets/shaders/unlit.frag")
+    unlit = Shader("./assets/shaders/unlit.vert", "./assets/shaders/unlit.frag")
 
     camera = Camera(wnd, 70, 1, 100)
     camera.transform.position.z = -3
@@ -24,10 +24,10 @@ def main():
 
         wnd.clear()
 
-        s.use()
-        s.pass_mat4("mvp", camera.get_mvp(trans))
-        s.pass_vec3("fragColor", Vector3(1, 1, 1))
-        m.draw()
+        unlit.use()
+        unlit.pass_mat4("mvp", camera.get_mvp(trans))
+        unlit.pass_vec3("fragColor", Vector3(1, 1, 1))
+        quad.draw()
 
         wnd.swap_buf()
 
